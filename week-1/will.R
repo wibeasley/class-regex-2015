b1091d25f30625cd822c6d7fa70553a18645f801
Will Beasley <wibeasley@hotmail.com>
2015-12-03 12:18:33 -0600

    State of examples at class's end

--- week-1/will.R
+++ week-1/will.R
@@ -2,3 +2,13 @@ subject <-
   "2016-2018 Muthen & Muthen
   1998-2014 Muthen & Muthen
   Muthen & Muthen 1998-2020"
+
+subject <- c(
+  "2016-2018 Muthen & Muthen",
+  "1998-2014 Muthen & Muthen",
+  "Muthen & Muthen 1998-2020"
+  )
+
+grep("[7]", subject,  perl=T)
+grep("[20]", subject, value=T, perl=T)
+
