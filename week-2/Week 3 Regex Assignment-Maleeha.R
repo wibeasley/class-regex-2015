e1e3d772d6a61c530162d332ebcd9d5f50f2a654
Maleeha Shahid <Maleeha-Shahid@ouhsc.edu>
2015-12-15 15:18:38 -0600

    RegexHomeWorkWeek3/3.1
    Just trying a slight hand at "Maximum number of Iterations for H1"

---
+++ week-2/Week 3 Regex Assignment-Maleeha.R
@@ -0,0 +1,31 @@
+# Code:3.1 (Regex Assignment)
+  
+  subject <- 
+  "Estimator                                                      MLF
+Information matrix                                        OBSERVED
+Maximum number of iterations                                   100
+Convergence criterion                                    0.100D-05
+Maximum number of EM iterations                               1000
+Convergence criteria for the EM algorithm
+Loglikelihood change                                   0.100D-02
+Relative loglikelihood change                          0.100D-05
+Derivative                                             0.100D-03
+Minimum variance                                         0.100D-03
+Maximum number of steepest descent iterations                   20
+Maximum number of iterations for H1                           2000
+Convergence criterion for H1                             0.100D-04
+Optimization algorithm                                         EMA"
+
+#regex_estimator <- "(?s).*?Estimator\\s+(\\w+).*"
+#estimator <- gsub(regex_estimator, "\\1", subject, perl=T)
+
+#print(estimator)
+
+# (?s)  is toggling an option. It's not a capturing group. As it says on page 40 of our book:
+#(?s)> is the mode modifier for "dot matches line breaks" mode in .NET, Java, XRegExp,
+#PCRE, Perl, and Python
+
+regex_IterationsH1 <- "(?s).*Maximum number of iterations for H1(?s).*?(\\d+).*"
+IterationsH1<- gsub(regex_IterationsH1, "\\1", subject, perl=T)
+
+print(IterationsH1)
