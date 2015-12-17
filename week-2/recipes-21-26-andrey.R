e5ba04f28c33d6ba57cbc59afaeb59bf9fa4bc9b
Andrey Koval <koval.andrey@gmail.com>
2015-12-17 13:30:19 -0800

    update

--- week-2/recipes-21-26-andrey.R
+++ week-2/recipes-21-26-andrey.R
@@ -19,23 +19,61 @@ cat(s3 <- "\\]^_`{|}~.") # the first wack is escaped, works
 print(s3) # however it stores the first wack
 (subject <- paste0(s1,s2,s3)) # proper subject
 
-# subjects <- strsplit(subject, split=" ")
-regex <- "The punctuation characters in the ASCII table are: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~."
-(x <- grep(pattern=regex, subject, value=T, perl=T))
+# define pattern to match in the string
+matchthis <- "The punctuation characters in the ASCII table are: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~."
+(x <- grep(pattern=matchthis, subject, value=T, perl=T))
 
 
 # @knitr 2_2 -----
 # Match non-printable character
-
-# I don't understand how the subject is supposed to look here
+# Most common non-printable characters (for me)
+# newline  : \n or \r or \n\r
+# tab: \t
 
 subject <-
 "aaac
 bbb"
-subject
 
-pattern <- "(.?)\n\r?(.+)"
-pattern <- "(\\w+)\\n(\\w+)" # any number of word character, line break, any nymber of word character
+subject # notice how R puts a wack where newline should be
+cat(subject) # check how it actually gets printed
+
+pattern <- "\n\r?" # match the newline
+gsub(pattern, "XX", subject) # replaces newline with XX
+
+# match newline and any ONE character to the LEFT of it
+pattern <- "(.)\n\r?"
+gsub(pattern, "XX", subject)
+
+# match newline and any TWO characters to the LEFT of it
+pattern <- "(.{2})\n\r?"
+# {} how many of the preceding character?
+gsub(pattern, "XX", subject)
+
+# match newline and any from TWO to FOUR characters to the LEFT of it
+pattern <- "(.{2,4})\n\r?"
+gsub(pattern, "XX", subject)
+
+# match newline and any number characters to the LEFT of it
+pattern <- "(.+)\n\r?"
+gsub(pattern, "XX", subject)
+
+
+# matches one character immediately preceding the newline
+pattern <- "(.?)\n\r?"
+# () defines the 1st capturing group
+# . matches any character except newline
+# ?  quantifies: between zero and one time, as many times as possible, giving back as needed [greedy]
+gsub(pattern, "XX", subject)
+
+# alternative to above
+# finds the single character immediately preceding the newline
+# \w match any word character [a-zA-Z0-9_]
+pattern <- "(\\w)\n\r?" # note the first wack is for R
+gsub(pattern, "XX", subject)
+
+
+pattern <- "(.?)\n\r?(.+)" # newline is optional
+pattern <- "(\\w+)\\n(\\w+)" # alternative, but newline is not optional
 gsub(pattern, "\\2zzz\\1yyy\\2", subject) # sub - for substitution
 
 # @knitr 2_3 -----
